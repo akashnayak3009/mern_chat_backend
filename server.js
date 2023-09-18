044b2fb288a772e8463f572444a896d2df9cdde0
akash <akashnayak3009@gmail.com>
2023-09-19 02:35:28 +0530

    message api

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import colors from 'colors';
 import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from './routes/chatRoutes.js'
+import messageRoutes from './routes/messageRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 import cors from "cors";
 
@@ -17,6 +18,7 @@ app.use(cors());
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message',messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
