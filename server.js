ad51679f109ce67273b98c40f65d4cd3409f6e2c
Akash kumar Nayak <akashnayak3009@gmail.com>
2023-09-23 13:36:06 +0530

    second commit user id

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import { chats } from './data/data.js';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import colors from 'colors';
+import express from "express";
+import { chats } from "./data/data.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
 import userRoutes from "./routes/userRoutes.js";
-import chatRoutes from './routes/chatRoutes.js';
-import messageRoutes from './routes/messageRoutes.js';
-import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
-import cors from 'cors';
-import http from 'http';
-import { Server } from 'socket.io';
-import bodyParser from 'body-parser';
+import chatRoutes from "./routes/chatRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import bodyParser from "body-parser";
 
 dotenv.config();
 connectDB();
@@ -22,11 +22,10 @@ app.use(cors());
 // Use body-parser middleware to parse JSON data
 app.use(bodyParser.json());
 
-
 // Routes
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 // Error handling
 app.use(notFound);
@@ -37,18 +36,23 @@ const port = process.env.PORT;
 const server = http.createServer(app);
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`.yellow.bold.underline);
+    console.log(`Server is running on port ${port}`.yellow.bold.underline);
 });
 
-//  Socket.io server 
+//  Socket.io server
 const io = new Server(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-  },
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+        // credentials: true,
+    },
 });
 
-io.on("connection",(socket)=>{
-    console.log("connected to socket.io".red.bold.italic)
-})
+io.on("connection", (socket) => {
+    console.log("connected to socket.io".red.bold.italic);
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+    });
+});
